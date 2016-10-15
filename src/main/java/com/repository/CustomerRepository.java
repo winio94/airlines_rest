b51d176a7de9d8a3b2c4c5f27035d2a965d20704
winio94 <winio94@gmail.com>
2016-10-15 18:38:07 +0200

    enabling security for customers and flights

--- src/main/java/com/repository/CustomerRepository.java
+++ src/main/java/com/repository/CustomerRepository.java
@@ -3,10 +3,14 @@ package com.repository;
 import com.domain.Customer;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 /**
  * Created by Michał on 2016-10-02.
  */
+
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public interface CustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+
     Customer findByLastName(@Param("name") String name);
 }
