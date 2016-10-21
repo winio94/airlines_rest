afb9cb9a45a68691e035b9f126595bb36d414676
winio94 <winio94@gmail.com>
2016-10-21 16:17:19 +0200

    security configuration cleanups

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -1,25 +1,15 @@
 package com;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.filter.CorsFilter;
 
 import java.security.Principal;
 
 /**
  * Created by Michał on 2016-10-01.
  */
-
 @SpringBootApplication
 @RestController
 public class Application {
@@ -31,37 +21,5 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Configuration
-    @EnableWebSecurity
-    protected class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-        @Autowired
-        private CorsFilter corsBean;
-
-        @Override
-        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-            auth.inMemoryAuthentication()
-                    .withUser("admin").password("admin").roles("USER", "ADMIN").and()
-                    .withUser("user").password("user").roles("USER");
-        }
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.addFilterBefore(corsBean, ChannelProcessingFilter.class)
-                    .csrf().disable()
-                    .authorizeRequests()
-                    .antMatchers(HttpMethod.PUT, "/flights").access("hasRole('ROLE_ADMIN')")
-                    .antMatchers(HttpMethod.POST, "/flights").access("hasRole('ROLE_ADMIN')")
-                    .antMatchers(HttpMethod.DELETE, "/flights").access("hasRole('ROLE_ADMIN')")
-                    .antMatchers(HttpMethod.GET, "/customers").access("hasRole('ROLE_ADMIN')")
-                    .antMatchers(HttpMethod.POST, "/customers").access("hasRole('ROLE_ADMIN')")
-                    .antMatchers(HttpMethod.PUT, "/customers").access("hasRole('ROLE_ADMIN')")
-                    .antMatchers(HttpMethod.DELETE, "/customers").access("hasRole('ROLE_ADMIN')")
-                    .antMatchers(HttpMethod.OPTIONS, "/*").permitAll()
-                    .and()
-                    .httpBasic();
-        }
     }
 }
---
+++ src/main/java/com/configuration/SecurityConfiguration.java
@@ -0,0 +1,46 @@
+package com.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * Created by Michał on 2016-10-21.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CorsFilter corsFilter;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("USER", "ADMIN").and()
+                .withUser("user").password("user").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(corsFilter, ChannelProcessingFilter.class)
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.PUT, "/flights").access("hasRole('ROLE_ADMIN')")
+                .antMatchers(HttpMethod.POST, "/flights").access("hasRole('ROLE_ADMIN')")
+                .antMatchers(HttpMethod.DELETE, "/flights").access("hasRole('ROLE_ADMIN')")
+                .antMatchers(HttpMethod.GET, "/customers").access("hasRole('ROLE_ADMIN')")
+                .antMatchers(HttpMethod.POST, "/customers").access("hasRole('ROLE_ADMIN')")
+                .antMatchers(HttpMethod.PUT, "/customers").access("hasRole('ROLE_ADMIN')")
+                .antMatchers(HttpMethod.DELETE, "/customers").access("hasRole('ROLE_ADMIN')")
+                .antMatchers(HttpMethod.OPTIONS, "/*").permitAll()
+                .and()
+                .httpBasic();
+    }
+}
