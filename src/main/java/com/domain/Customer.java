1d60c6ffcb1bc0f50467e22bf26ce787a77dd230
winio94 <winio94@gmail.com>
2016-10-23 15:09:35 +0200

    add airport reference to flight entity

--- src/main/java/com/domain/Customer.java
+++ src/main/java/com/domain/Customer.java
@@ -17,6 +17,14 @@ public class Customer {
     private String firstName;
     private String lastName;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getFirstName() {
         return firstName;
     }
