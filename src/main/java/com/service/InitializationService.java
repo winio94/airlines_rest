2825f85d15d2b11547c2261f3abb666f649cf8bc
winio94 <winio94@gmail.com>
2016-10-28 12:47:34 +0200

    initialize airports at app start

--- src/main/java/com/service/InitializationService.java
+++ src/main/java/com/service/InitializationService.java
@@ -1,15 +1,19 @@
 package com.service;
 
+import com.domain.Airport;
 import com.domain.Customer;
 import com.domain.Flight;
+import com.repository.AirportRepository;
 import com.repository.CustomerRepository;
 import com.repository.FlightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import static java.lang.String.valueOf;
 
@@ -20,18 +24,34 @@ import static java.lang.String.valueOf;
 @Component
 public class InitializationService {
 
+    public static final String CITIES_FILE_PATH = "src/main/resources/cities.txt";
+    private final List<String> cities = FileUtil.readAllLinesFrom(CITIES_FILE_PATH, "\\s+");
+
     @Autowired
     private CustomerRepository customerRepository;
 
     @Autowired
     private FlightRepository flightRepository;
 
+    @Autowired
+    AirportRepository airportRepository;
+
+    public InitializationService() throws IOException {
+    }
+
     @PostConstruct
     private void initialize() {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < cities.size(); i++) {
+            airportRepository.save(airport(i));
             customerRepository.save(customer(i));
             flightRepository.save(flight(i));
         }
+    }
+
+    private Airport airport(int i) {
+        Airport airport = new Airport();
+        airport.setCity(cities.get(i));
+        return airport;
     }
 
     private Flight flight(int counter) {
