da6055e069b3e19cf4438dc7d68bbdd09c68b5c5
winio94 <winio94@gmail.com>
2016-11-13 10:07:22 +0100

    create constants for messages and numbers

--- src/main/java/com/util/constants/MessageConstants.java
+++ src/main/java/com/util/constants/MessageConstants.java
@@ -3,7 +3,11 @@ package com.util.constants;
 /**
  * Created by Michał on 2016-11-11.
  */
-public interface Constants {
+public interface MessageConstants {
+    ///////////////////////////////SUCCESS MESSAGES//////////////////////////////////////
+    public static final String TICKET_MAIL_SUBJECT = "Airline Reservation System. Ticket FOR ";
+
+    ////////////////////////////////ERROR MESSAGES///////////////////////////////////////
     public static final String RESERVATION_VALIDATION_MESSAGE = "Could not create reservation. Either contact information or reservation date was missing.";
     public static final String NULL_RESERVATION_MESSAGE = "Could not persist NULL reservation.";
 }
---
+++ src/main/java/com/util/constants/NumberConstants.java
@@ -0,0 +1,8 @@
+package com.util.constants;
+
+/**
+ * Created by Michał on 2016-11-13.
+ */
+public interface NumberConstants {
+    public static final int SENDING_MAIL_INTERVAL = 5000;
+}
