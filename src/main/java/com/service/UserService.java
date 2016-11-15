e3db0a40496dcc09b00212aa28e53d730d25dafc
winio94 <winio94@gmail.com>
2016-11-15 19:12:07 +0100

    create service for User

---
+++ src/main/java/com/service/UserService.java
@@ -0,0 +1,20 @@
+package com.service;
+
+import com.domain.User;
+
+import java.util.List;
+
+/**
+ * Created by Michał on 2016-11-14.
+ */
+public interface UserService {
+    User getUserById(Long id);
+
+    User getUserByEmail(String email);
+
+    User getUserByUserName(String userName);
+
+    List<User> getAllUsers();
+
+    User create(User user);
+}
