96a26d65735bec4fec97d00936351ed97c2569ad
winio94 <winio94@gmail.com>
2016-11-15 19:11:48 +0100

    create repository for User

---
+++ src/main/java/com/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.repository;
+
+import com.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+/**
+ * Created by Michał on 2016-11-14.
+ */
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findUserByEmail(@Param("email") String email);
+
+    User findUserByUserName(@Param("userName") String userName);
+}
