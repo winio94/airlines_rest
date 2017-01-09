72809b6179c3e9ef0d351e8ff159445f7bb9dc74
winio94 <winio94@gmail.com>
2017-01-09 19:23:51 +0100

    secure retrieving customers

--- src/main/java/com/repository/CustomerRepository.java
+++ src/main/java/com/repository/CustomerRepository.java
@@ -1,9 +1,13 @@
 package com.repository;
 
 import com.domain.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
@@ -22,4 +26,20 @@ public interface CustomerRepository extends PagingAndSortingRepository<Customer,
     List<Customer> findCustomersByPartOfLastName(@Param("lastName") String lastName);
 
     Customer findCustomerByUserId(@Param("id") Long id);
+
+    @Override
+    @PreAuthorize("hasAuthority('ADMIN')")
+    Iterable<Customer> findAll();
+
+    @Override
+    @PreAuthorize("hasAuthority('ADMIN')")
+    Iterable<Customer> findAll(Iterable<Long> iterable);
+
+    @Override
+    @PreAuthorize("hasAuthority('ADMIN')")
+    Iterable<Customer> findAll(Sort sort);
+
+    @Override
+    @PreAuthorize("hasAuthority('ADMIN')")
+    Page<Customer> findAll(Pageable pageable);
 }
